6edd38275b77243df028f3c3e5b754a4218a2647
iliakozmava <kozmavarevazi@gmail.com>
2025-07-10 10:05:12 -0700

    commit

---
+++ day 01/classwork/classwork.py
@@ -0,0 +1,15 @@
+# 1. String-ი არის ისეთი რამე რომელიც უნდა იყოს პრჭყალებში მოთავსებული მაგალითად --> print("ilia kozmava")
+
+# 2. print-ის ფუნქციით შეგილია ტერმინალში დაბეჭდო ნებისმიერი რამე --> print(250) / print("iliakozmava")
+
+# 3. 
+print("ilia")
+print(13)
+
+# 4. ცელადებს ვიყენებთ ჩვენი პრორესის შესანახად --> name = "ilia" / print(name)
+
+# 5.
+name = "ilia"
+surname = "kozmava"
+print(name)
+print(surname)
---
+++ day 01/homework/homework.py
@@ -0,0 +1,12 @@
+name = "ilia"
+age = 13
+surname = "kozmava"
+city = "tbilisi"
+
+
+mother = "qristine"
+father = "rezi"
+me = "ilia"
+print(mother)
+print(father)
+print(me)
