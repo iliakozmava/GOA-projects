47e70676a936c6d92f8517fdf4fd11dd9ee210df
iliakozmava <kozmavarevazi@gmail.com>
2025-08-23 17:25:40 +0400

    commit

---
+++ day 07/classwork/cw.py
@@ -0,0 +1,74 @@
+# 1
+# input ნიშნავს რომ მომხმარებელი რაღაცას წერს ანუ შეყავს
+# output ნიშნავს რომ პროგრამა რაღაცას აჩვენებს ეკრანზე
+
+name = input("input name: ")
+print("Welcome" + name)
+
+# 2
+# კონკატინაცია ნიშნავს ორი ან მეტი ტექსტის შეერთებას
+name2 = "ilia"
+surname = "kozmava"
+print(name2 + surname)
+
+# 3
+text = input("write text: ")
+print("you wrote" + text)
+
+# 4
+name3 = input("name: ")
+surname2 = input("surname: ")
+age = input("age: ")
+location = input("location: ")
+color = input("favorite color: ")
+car = input("favourite car: ")
+
+dialogue = "my name is" + name3 + " " + surname2 + " " + "i am" + age + " " + "years old" + " " + "i live in" + " " + location + " " + "my favourite color is" + color + " " + "and my favourite car is" + car
+
+print(dialogue)
+
+# 5
+
+test1 = "5"
+test2 = "10"
+test3 = "15"
+test4 = "20"
+test5 = "25"
+
+num1 = int(test1)
+num2 = int(test2)
+num3 = int(test3)
+num4 = int(test4)
+num5 = int(test5)
+
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+
+# 6
+
+num11 = 3
+num22 = 6
+num33 = 9
+num44 = 12
+num55 = 15
+
+test11 = str(num11)
+test22 = str(num22)
+test33 = str(num33)
+test44 = str(num44)
+test55 = str(num55)
+
+print(test11)
+print(test22)
+print(test33)
+print(test44)
+print(test55)
+
+# 7
+
+number = int(input("write a number "))
+equal = number + 15
+print("you're number + 15 is " + str(equal))
---
+++ day 07/homework/hw.py
@@ -0,0 +1,63 @@
+ # 1
+name = input("write your name")  
+print("hello " + name)  
+
+# 2
+first = "ilia"  
+last = "kozmava"  
+full = first + " " + last  
+print(full) 
+
+# 3
+# string number  
+a = "10"  
+
+# real number  
+b = 5  
+
+# change string to number  
+a_num = int(a)  
+
+# plus  
+print(a_num + b)  # 15  
+
+# minus  
+print(a_num - b)  # 5  
+
+# times  
+print(a_num * b)  # 50  
+
+# divide  
+print(a_num / b)  # 2 point 0  
+
+# 4  
+h = input("5'6")  
+h_float = float(h)  
+print(type(h_float))
+
+# 5
+# მონაცემების გარდაქმნა ნიშნავს ერთი ტიპის მონაცემის შეცვლას სხვა ტიპად  
+# მაგალითად სტრინგის (ტექსტის) გადაკეთება რიცხვად  
+# ან რიცხვის გადაკეთება სტრინგად  
+
+# 6
+
+x = 3  
+y = 1  
+z = x + y 
+print(z)
+
+s = "7"  
+n = int(s)  
+print(n + 3)
+
+ # ტიპის შეცვლის ფუნქციები  
+
+# int() - ქმნის რიცხვს წერტილის გარეშე  
+# float() - ქმნის რიცხვს წერტილით  
+# str() - ქმნის სიტყვას ანუ ტექსტს  
+# bool() - ქმნის True ან False მნიშვნელობას  
+# list() - ქმნის სიას  
+# tuple() - ქმნის უცვლელ სიას  
+# set() - ქმნის უნიკალურ ელემენტების ნაკრებს  
+# dict() - ქმნის მონაცემებს გასაღებით და მნიშვნელობით  
