22ea47bd1bc07ffd20d881e30495c003621cea2c
iliakozmava <kozmavarevazi@gmail.com>
2025-09-16 20:00:39 +0400

    commit

---
+++ day 11/hw.py
@@ -0,0 +1,24 @@
+# 2
+for number in range(1, 6):
+    print(number)
+
+# 3
+for number in range(1, 101):
+    print(number)
+
+# 4
+for number in range(1, 100):
+    if number % 5 == 0:
+        continue
+    print(number)
+
+# 5
+for number in range(5, 88):
+    if number % 2 == 0:
+        print(number)
+
+# 6
+name = input("What is your name ")
+
+for i in range(15):
+    print(name)
