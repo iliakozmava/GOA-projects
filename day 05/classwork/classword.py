869268154c118bb6320fdebf092f1dc072dbe58c
iliakozmava <kozmavarevazi@gmail.com>
2025-10-10 18:37:06 +0400

    commit

---
+++ day 05/classwork/classword.py
@@ -0,0 +1,9 @@
+name = "ilia"
+age = "13"
+favoritemovie = "harry potter"
+friendsname = "bob"
+
+print("my name is " + name)
+print("my age is " + age)
+print("my favorite movie is " + favoritemovie)
+print("my friends name is " + friendsname)
---
+++ day 05/homework/homework.py
@@ -0,0 +1,65 @@
+# 2) კომენტარი გვიჩვენებს რას ნიშნავს კოდი
+# კარგია რადგან გვახსენებს რას ვწერთ
+
+# 3)
+my_name = "ilia"
+my_age = "13"
+my_city = "tbilisi"
+my_color = "blue"
+my_game = "minecraft"
+my_pet = "dog"
+my_school = "39 sajaro skola"
+my_friend = "nikolozi"
+my_hobby = "coding"
+my_food = "xawapuri"
+
+print(my_name)
+print(my_age)
+print(my_city)
+print(my_color)
+print(my_game)
+print(my_pet)
+print(my_school)
+print(my_friend)
+print(my_hobby)
+print(my_food)
+
+# 4)
+name1 = "ilia"
+age_num = "13"
+my_favmov = "spiderman"
+my_friends_name = "nikolozi"
+
+print("my name is " + name1)
+print("my age is " + age_num)
+print("my movie is " + my_favmov)
+print("my friend is " + my_friends_name)
+
+# 5)
+gamarjoba = "gamarjoba"
+rogorxar = "rogor xar"
+winadadeba = gamarjoba + rogorxar
+print(winadadeba)
+
+kargi = "kargi"
+dge = "dge"
+winadadeba2 = kargi + dge
+print(winadadeba2)
+
+chemi = "chemi"
+megobari = "megobari"
+winadadeba3 = chemi + megobari
+print(winadadeba3)
+
+vswavlob = "vswavlob"
+goashi = "goashi"
+winadadeba4 = vswavlob + goashi
+print(winadadeba4)
+
+m = "fun "
+n = "time"
+o = m + n
+print(o)
+
+# 6)
+# snake_case არის როცა სიტყვებს შორის არის ქვედა ხაზი მაგალითად my_name
