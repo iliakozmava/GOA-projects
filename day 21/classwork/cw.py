9cd9a0d88b27509a60e703f995757eeba37a3cbb
iliakozmava <kozmavarevazi@gmail.com>
2025-12-06 18:41:31 +0400

    commit

---
+++ day 21/classwork/cw.py
@@ -0,0 +1,29 @@
+# 1
+# indexing არის როცა ვიღებთ ერთ ელემენტს ნომრით
+# slicing არის როცა ვიღებთ ელემენტებს დიაპაზონით
+
+# 2
+# ფუნქცია არის კოდი რომელსაც ვიძახებთ სახელით
+# მაგალითად print len type
+
+# 3
+currency_symbols = ["$","€","£", "¥", "₹","₩","₽","₺","₦"]
+
+print(currency_symbols[3:-1])
+
+print(currency_symbols[::-1])
+
+print(currency_symbols[::2])
+
+# 4
+surname = "kozmava"
+
+print(surname[-4:])
+print(surname[:3])
+
+# 5
+names = ["ilia", "nika", "lika", "bika", "cika", "nike"]
+
+names[-3] = "bike"
+
+print(names)
---
+++ day 21/homework/hw.py
@@ -0,0 +1,32 @@
+# 1
+
+cars = ["bmw", "audi", "mecides", "toiota", "honda", "fordi", "kia", "hiundai"]
+
+print(cars[0:5])
+
+print(cars[-1:-5:-1])
+
+cars[-1] = "ziguli"
+print(cars)
+
+print(cars[::3])
+
+
+# 2
+
+name = ["i", "l", "i", "a", "k", "o"]
+print(name[::2])
+
+
+# 3
+
+food = ["xinkali", "xawapuri", "lobiani", "simindi", "katleti"]
+
+food[-1] = "mwadi"
+print(food)
+
+print(food[::-1])
+
+print(food[0:3])
+
+print(food[2])
